2cd76700c0e850db4e24085f1da5afff2144c76a
Anastasia <71445237+NasyaM@users.noreply.github.com>
2024-11-28 16:13:33 +0300

    Приложение для блога (задание 2)

---
+++ my_site/README.md
@@ -0,0 +1,9 @@
+# Процесс установки
+
+# 1. Поместить папку проекта в системную папку System32
+# 2. В командной строке администратора перейти в папку проекта "cd my_site" 
+# 3. Установить virtualenv "pip install virtualenv"
+# 4. Создать виртуальную среду "virtualenv myenv" и активировать "myenv\Scripts\activate.bat"
+# 5. Установить django внутри виртуального окружения "pip install django"
+# 6. Запустить сервер "python manage.py runserver"
+# 7. Проверить работоспособность сайта по ссылке http://localhost:8000/
---
+++ my_site/blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Админ',
+        'title': 'Это первый пост',
+        'content': 'Содержание первого поста.',
+        'date_posted': '12 мая, 2022'
+    },
+    {
+        'author': 'Пользователь',
+        'title': 'Это второй пост',
+        'content': 'Подробное содержание второго поста.',
+        'date_posted': '13 мая, 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'Блог Барни'})
+
+def post(request):
+    return render(request, 'blog/post.html', {'title': 'Блог Барни'})
